12b7f9ac13195aab35935b4b0c5c98787421f959
Utsav-Ladani <201901076@daiict.ac.in>
2025-05-17 21:48:34 +0530

    Highlight active link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import PlusIcon from "../icons/PlusIcon";
 
 export default function Sidebar({ chatRootNodes, onNewChat }) {
     return (
-        <aside className="bg-gray-100 py-4 w-64 border-r border-gray-200">
+        <aside className="flex flex-col bg-gray-100 py-4 w-64 border-r border-gray-200 h-screen">
             <header className="flex justify-between items-center mb-6 px-4">
                 <Link to="/">
                     <h2 className="text-2xl font-bold">Chat Trees 🌳</h2>
@@ -18,12 +18,16 @@ export default function Sidebar({ chatRootNodes, onNewChat }) {
             <ul className="flex flex-col gap-2 overflow-y-auto px-2">
                 {chatRootNodes.map((node) => (
                     <li key={node.id} className="p-0">
-                        <Link
-                            className="px-2 py-1 hover:bg-gray-200 hover:cursor-pointer rounded block capitalize"
+                        <NavLink
                             to={`/chat/${node.id}`}
+                            className={({ isActive }) =>
+                                `px-2 py-1 hover:cursor-pointer rounded block capitalize ${
+                                    isActive ? "bg-black text-white" : "hover:bg-gray-200"
+                                }`
+                            }
                         >
                             {node.user || 'Untitled'}
-                        </Link>
+                        </NavLink>
                     </li>
                 ))}
             </ul>
