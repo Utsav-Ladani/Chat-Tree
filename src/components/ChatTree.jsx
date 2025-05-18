d2cd2df52c107f1425d4c303ba4808c7a2e204a1
Utsav-Ladani <201901076@daiict.ac.in>
2025-05-18 12:15:50 +0530

    Prevent macOS scroll bounce

--- src/components/ChatTree.jsx
+++ src/components/ChatTree.jsx
@@ -13,8 +13,21 @@ export default function ChatTree({ chatRootNode, parentRef, onAddChild, reRender
     const handleZoomIn = () => setZoom(z => Math.min(z + 0.1, 1));
     const handleZoomOut = () => setZoom(z => Math.max(z - 0.1, 0.4));
 
+    const handleWheel = (e) => {
+        e.preventDefault();
+
+        if (e.deltaY < 0) {
+            setZoom(z => Math.min(z + 0.05, 1));
+        } else if (e.deltaY > 0) {
+            setZoom(z => Math.max(z - 0.05, 0.4));
+        }
+    };
+
     return (
-        <main className="p-4 overflow-hidden relative">
+        <main
+            className="p-4 overflow-hidden relative"
+            onWheel={handleWheel}
+        >
             <div className="absolute bottom-4 right-4 z-1 flex items-center gap-2 bg-gray-100 border border-gray-400 rounded-md shadow-xl p-2">
                 <button onClick={handleZoomIn} className="p-2 bg-black text-white border rounded hover:bg-gray-300 hover:text-black hover:border-gray-400">
                     <ZoomIn size={16} />
